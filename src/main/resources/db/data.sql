328a2ef61ca1ad0b0b427ec6c08a84b13af71b72
tineojf <92743762+tineodev@users.noreply.github.com>
2025-01-21 13:17:52 -0500

    feat: add schema & data sql

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,14 @@
+-- Inserciones para la tabla moneda
+INSERT INTO `moneda` (`codigo`, `descripcion`) VALUES
+('USD', 'Dolar americano'),
+('PEN', 'Sol peruano');
+
+-- Inserciones para la tabla tipo_cambio
+INSERT INTO `tipo_cambio` (`moneda_origen`, `moneda_destino`, `compra`, `venta`) VALUES
+(1, 2, 3.5, 3.6),
+(2, 1, 0.28, 0.29);
+
+-- Inserciones para la tabla transaccion
+INSERT INTO `transaccion` (`tipo_transaccion`, `monto_origen`, `moneda_origen`, `moneda_destino`, `monto_destino`) VALUES
+('COMPRA', 100.00, 1, 2, 350.00),
+('VENTA', 100.00, 2, 1, 28.00);
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,1 +1,56 @@
-m
+-- Delete all tables
+DROP TABLE IF EXISTS `transaccion`;
+DROP TABLE IF EXISTS `tipo_cambio`;
+DROP TABLE IF EXISTS `moneda`;
+
+-- -----------------------------------------------------
+ -- Table `moneda`
+ -- -----------------------------------------------------
+ CREATE TABLE IF NOT EXISTS `moneda` (
+   `moneda_id` BIGINT NOT NULL AUTO_INCREMENT,
+   `codigo` VARCHAR(3) NOT NULL UNIQUE,
+   `descripcion` VARCHAR(45) NOT NULL,
+   PRIMARY KEY (`moneda_id`));
+
+ -- -----------------------------------------------------
+ -- Table `tipo_cambio`
+ -- -----------------------------------------------------
+ CREATE TABLE IF NOT EXISTS `tipo_cambio` (
+   `tipo_cambio_id` BIGINT NOT NULL AUTO_INCREMENT,
+   `moneda_origen` BIGINT NOT NULL,
+   `moneda_destino` BIGINT NOT NULL,
+   `compra` DECIMAL(10,4) NOT NULL,
+   `venta` DECIMAL(10,4) NOT NULL,
+   `actualizacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (`tipo_cambio_id`),
+   INDEX `fk_tipo_cambio_moneda_origen_idx` (`moneda_origen` ASC) VISIBLE,
+   INDEX `fk_tipo_cambio_moneda_destino_idx` (`moneda_destino` ASC) VISIBLE,
+   CONSTRAINT `fk_tipo_cambio_moneda_origen`
+     FOREIGN KEY (`moneda_origen`) REFERENCES `moneda` (`moneda_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+   CONSTRAINT `fk_tipo_cambio_moneda_destino`
+     FOREIGN KEY (`moneda_destino`) REFERENCES `moneda` (`moneda_id`) ON DELETE CASCADE ON UPDATE CASCADE);
+
+ -- -----------------------------------------------------
+ -- Table `transaccion`
+ -- -----------------------------------------------------
+ CREATE TABLE IF NOT EXISTS `transaccion` (
+   `transaccion_id` INT NOT NULL AUTO_INCREMENT,
+   `tipo_transaccion` ENUM('VENTA', 'COMPRA') NOT NULL,
+   `monto_origen` DECIMAL(10,2) NOT NULL,
+   `moneda_origen` BIGINT NOT NULL,
+   `moneda_destino` BIGINT NOT NULL,
+   `monto_destino` DECIMAL(10,2) NOT NULL,
+   `fecha_hora_transaccion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (`transaccion_id`),
+   INDEX `fk_transaccion_moneda_origen_idx` (`moneda_origen` ASC) VISIBLE,
+   INDEX `fk_transaccion_moneda_destino_idx` (`moneda_destino` ASC) VISIBLE,
+   CONSTRAINT `fk_transaccion_moneda_origen`
+     FOREIGN KEY (`moneda_origen`) REFERENCES `moneda` (`moneda_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+   CONSTRAINT `fk_transaccion_moneda_destino`
+     FOREIGN KEY (`moneda_destino`) REFERENCES `moneda` (`moneda_id`) ON DELETE CASCADE ON UPDATE CASCADE);
+
+
+
+
+
+
