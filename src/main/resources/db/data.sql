8ac23e277a1299120d01100f2f001ce1ad32fe15
tineojf <92743762+tineodev@users.noreply.github.com>
2025-01-21 16:11:03 -0500

    fix: sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -5,8 +5,7 @@ INSERT INTO `moneda` (`codigo`, `descripcion`) VALUES
 
 -- Inserciones para la tabla tipo_cambio
 INSERT INTO `tipo_cambio` (`moneda_origen`, `moneda_destino`, `compra`, `venta`) VALUES
-(1, 2, 3.5, 3.6),
-(2, 1, 0.28, 0.29);
+(1, 2, 3.5, 3.6);
 
 -- Inserciones para la tabla transaccion
 INSERT INTO `transaccion` (`tipo_transaccion`, `monto_origen`, `moneda_origen`, `moneda_destino`, `monto_destino`, `tipo_cambio`) VALUES
