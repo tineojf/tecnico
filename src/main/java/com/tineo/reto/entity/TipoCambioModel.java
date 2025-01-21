c0dc3f6a468e96e94d482e0015e9960392bf71fa
tineojf <92743762+tineodev@users.noreply.github.com>
2025-01-21 16:13:27 -0500

    feat: set column names

--- src/main/java/com/tineo/reto/entity/TipoCambioModel.java
+++ src/main/java/com/tineo/reto/entity/TipoCambioModel.java
@@ -2,6 +2,7 @@ package com.tineo.reto.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -11,9 +12,16 @@ import java.time.LocalDateTime;
 public class TipoCambioModel {
     @Id
     private Long tipoCambioId;
+
+    @Column("moneda_origen")
     private Long monedaOrigenId;
+
+    @Column("moneda_destino")
     private Long monedaDestinoId;
+
     private Double compra;
+
     private Double venta;
+
     private LocalDateTime actualizacion;
 }
